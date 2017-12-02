852c9004eec2345f749bc2b93dcecb398cda83de
Nathan Baylon <nathanbaylon@gmail.com>
2017-12-03 00:41:39 +1100

    Line Spacing

--- MakingWindow.c
+++ MakingWindow.c
@@ -30,8 +30,7 @@ int main(int argc, char *argv[]) {
 			screenSurface = SDL_GetWindowSurface(window);
 
 			//Fill the surface white
-			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF,
-														 0xFF));
+			SDL_FillRect(screenSurface, NULL, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
 
 			//Update the surface
 			SDL_UpdateWindowSurface(window);
